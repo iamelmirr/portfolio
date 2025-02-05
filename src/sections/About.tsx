1baa6d8a1b08b331b36aeb6f382003d56a53e339
iamelmirr <besirovicelmir36@gmail.com>
2025-02-05 18:10:24 +0100

    Portfolio v1

---
+++ src/sections/About.tsx
@@ -0,0 +1,169 @@
+"use client"
+import { SectionHeader } from "@/components/SectionHeader";
+import { Card } from "@/components/Card";
+import StarIcon from "@/assets/icons/star.svg"
+import bookImage from "@/assets/images/book-cover.png"
+import Image from "next/image";
+import JavascriptIcon from "@/assets/icons/square-js.svg"
+import HTMLIcon from "@/assets/icons/html5.svg"
+import CssIcon from "@/assets/icons/css3.svg"
+import ReactIcon from "@/assets/icons/react.svg"
+import ChromeIcon from "@/assets/icons/chrome.svg"
+import GithubIcon from "@/assets/icons/github.svg"
+import { TechIcon } from "@/components/TechIcon";
+import mapImage from "@/assets/images/map.png"
+import smileEmoji from "@/assets/images/memoji-smile.png"
+import { CardHeader } from "@/components/CardHeader";
+import { ToolboxItems } from "@/components/ToolboxItems";
+import { motion } from "framer-motion";
+import { useRef } from "react";
+
+
+const toolboxItems = [
+  {
+    title: 'JavaScript',
+    iconType: JavascriptIcon,
+  },
+  {
+    title: 'HTML5',
+    iconType: HTMLIcon,
+  },
+  {
+    title: 'CSS3',
+    iconType: CssIcon,
+  },
+  {
+    title: 'React',
+    iconType: ReactIcon,
+  },
+  {
+    title: 'Chrome',
+    iconType: ChromeIcon,
+  },
+  {
+    title: 'Github',
+    iconType: GithubIcon,
+  },
+  
+  
+];
+
+
+const hobbies = [
+  {
+    title: 'Football',
+    emoji: '⚽',
+    left: '5%',
+    top: '5%',
+  },
+
+  {
+    title: 'Gaming',
+    emoji: '🎮',
+    left: '50%',
+    top: '5%',
+  },
+
+  
+
+  
+
+  {
+    title: 'Hiking',
+    emoji: '🥾',
+    left: '70%',
+    top: '45%',
+  },
+
+  {
+    title: 'Fitness',
+    emoji: '🏋️‍♂️',
+    left: '35%',
+    top: '40%',
+  },
+
+  {
+    title: 'Cycling',
+    emoji: '🚴‍♀️',
+    left: '5%',
+    top: '65%',
+  },
+  {
+    title: 'Coding',
+    emoji: '💻',
+    left: '10%',
+    top: '35%',
+  },
+  {
+    title: 'Traveling',
+    emoji: '🧳',
+    left: '45%',
+    top: '70%',
+  },
+  
+]
+
+
+
+export const AboutSection = () => {
+
+  const dragRef = useRef(null)
+
+  return (
+  <div id="about" className="py-20 lg:py-28">
+    <div className="container">
+
+    <SectionHeader 
+    eyebrow="About Me"
+    title="A Glimpse Into My World"
+    description="Learn more about who I am, what I do, and what inspires me"
+    />
+
+    <div className="mt-20 flex flex-col gap-8">
+      <div className="grid grid-cols-1 gap-8 md:grid-cols-5 lg:grid-cols-3">
+      <Card className="h-[320px] md:col-span-2 lg:col-span-1">
+        <CardHeader title="My Reads" description="Explore the books shaping my perspectives." />
+        <div className="w-40 mx-auto mt-2 md:mt-0">
+        <Image src={bookImage} alt="Book cover" />
+        </div>
+      </Card>
+      <Card className="h-[320px] md:col-span-3 lg:col-span-2">
+      <CardHeader className="" title="My Toolbox" description="Explore the technologies and tools I use to craft exceptional digital experiences." />
+        
+        <ToolboxItems items={toolboxItems} className="" itemsWrapperClassName="animate-move-left [animation-duration:30s]"/>
+        <ToolboxItems items={toolboxItems} itemsWrapperClassName="animate-move-right [animation-duration:15s]" className="mt-6"/>
+        
+      </Card>
+      </div>
+      <div className="grid grid-cols-1 gap-8 md:grid-cols-5 lg:grid-cols-3">
+      <Card className="h-[320px] p-0 flex flex-col md:col-span-3 lg:col-span-2">
+      <CardHeader title="Beyond the Code" description="Explore my interests and hobbies beyond the digital realm." className="px-6 py-6"/>
+      
+        <div className="relative flex-1" ref={dragRef}
+        >
+          {hobbies.map(hobby => (
+            <motion.div
+             key={hobby.title} className="inline-flex items-center gap-2 px-6 bg-gradient-to-r from-emerald-300 to-sky-400 rounded-full py-1.5 absolute cursor-grab" style={{
+              left: hobby.left,
+              top: hobby.top,
+            }} drag dragConstraints={dragRef}>
+              <span className="font-medium text-gray-950">{hobby.title}</span>
+              <span>{hobby.emoji}</span>
+            
+            </motion.div>
+          ))}
+        </div>
+      </Card>
+      <Card className="h-[320px] p-0 relative md:col-span-2 lg:col-span-1">
+        <Image src={mapImage} alt="Map" className="h-full w-full object-cover object-left-top"/>
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-20 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 after:content-[''] after:absolute after:inset-0 after:outline after:outline-2 after:outline-offset-2 after:rounded-full after:outline-gray-950/30">
+        <div className="absolute inset-0 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 -z-20 animate-ping [animation-duration:2s]"></div>
+        <div className="absolute inset-0 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 -z-10"></div>
+        <Image src={smileEmoji} alt="smiling emoji" className="size-20" />
+        </div>
+      </Card>
+      </div>
+    </div>
+    </div>
+  </div>)
+};
---
+++ src/sections/Projects.tsx
@@ -0,0 +1,107 @@
+import languageLabImage from "@/assets/images/language-lab-project.png"
+import shelfieImage from "@/assets/images/shelfie-project.png"
+import caffiendImage from "@/assets/images/caffiendproject.png"
+import Image from "next/image";
+import CheckIcon from "@/assets/icons/check-circle.svg";
+import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
+import grainImage from "@/assets/images/grain.jpg"
+import { SectionHeader } from "@/components/SectionHeader";
+import { Card } from "@/components/Card";
+
+const portfolioProjects = [
+  {
+    company: "Personal",
+    year: "2025",
+    title: "AI Interactive Language Tutor",
+    results: [
+      { title: "Integrated NLP with STT/TTS" },
+      { title: "Implemented real-time AI chat" },
+      { title: "User authentication and progress tracking" },
+    ],
+    link: "https://language1ab.netlify.app/",
+    image: languageLabImage,
+  },
+  {
+    company: "Personal",
+    year: "2024",
+    title: "Book Search Engine",
+    results: [
+      { title: "Integrated Google Books API" },
+      { title: "Implemented dynamic book filtering" },
+      { title: "Optimized search results display" },
+    ],
+    link: "https://shelfie-search.netlify.app/",
+    image: shelfieImage,
+  },
+  {
+    company: "Personal",
+    year: "2024",
+    title: "Caffeine Intake Tracker",
+    results: [
+      { title: "Health-based consumption alerts" },
+      { title: "Providing feedback system" },
+      { title: "Designed an intuitive user interface" },
+    ],
+    link: "https://caffiending.netlify.app/",
+    image: caffiendImage,
+  },
+];
+
+export const ProjectsSection = () => {
+  return <section id="projects" className="pb-16 lg:py-24">
+    <div className="container flex flex-col">
+      <SectionHeader 
+      eyebrow="Real-world Results"
+      title="Featured Projects"
+      description="See how I transformed concepts into engaging digital experiences."
+      />
+      
+      
+      <div className="flex flex-col mt-10 md:mt-20 gap-20">
+        {portfolioProjects.map((project, projectIndex) => (
+          <Card className="px-8 pt-8 pb-0 md:pt-12 md:px-10 lg:pt-16 lg:px-20 sticky top-16" key={project.title} style={{
+            top: `calc(64px + ${projectIndex * 40}px)`
+          }}>
+
+            <div className="lg:grid lg:grid-cols-2 lg:gap-16">
+              <div className="lg:pb-16">
+            
+              <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
+                <span> {project.company} </span>
+                <span>&bull;</span>
+                <span> {project.year} </span>
+              </div>
+             
+             <h3 className="font-serif text-2xl mt-2 md:mt-5 md:text-4xl">{project.title}</h3>
+             <hr className="border-t-2 border-white/5 mt-4 md:mt-5" />
+             <ul className="flex flex-col gap-4 mt-4 md:mt-5">
+              {project.results.map(results => (
+                <li key={results.title} className="flex gap-2 text-sm md:text-base text-white/50">
+                  <CheckIcon className="size-5 md:size-6" />
+                  <span>{results.title}</span>
+                </li>
+              ))}
+             </ul>
+             <a href={project.link} target="_blank">
+             <button className="bg-white text-gray-950 h-12 w-full md:w-auto px-6 rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8 hover:opacity-60 transition">
+              <span>Visit Live Site</span>
+              <ArrowUpRightIcon className="size-4" />
+             </button>
+             </a>
+             </div>
+             <div className="relative">
+             <Image src={project.image} alt={project.title} className="mt-8 -mb-4 md:-mb-0 lg:mt-0 lg:absolute lg:h-full lg:w-auto lg:max-w-none"  />
+             </div>
+             </div>
+          </Card>
+        ))}
+
+      </div>
+    <div className="flex flex-col pt-24 pb-30 items-center">  
+    <SectionHeader eyebrow="Explore More" title="See more on GitHub" description="Dive deeper into my projects and code"/>
+    <a href="https://github.com/iamelmirr" target="_blank" className="bg-white text-gray-950 h-12 w-max px-6 rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-12 hover:opacity-60 transition"><span>GitHub</span> <ArrowUpRightIcon className="size-4" /></a>
+    </div>
+    </div>
+
+  </section>;
+};
