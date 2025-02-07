fe48dcd4c565d67c69965998222c4949675b2424
iamelmirr <besirovicelmir36@gmail.com>
2025-02-07 16:45:05 +0100

    contact bug fixed

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -5,25 +5,23 @@ import grainImage from "@/assets/images/grain.jpg"
 
 export const ContactSection = () => {
   return (
-  <div id="contact" className="py-16 pt-12 lg:py-24 lg:pt-20">
-    <div className="container">
-      <div className="bg-gradient-to-r from-emerald-300 to-sky-400 text-gray-900 py-8 px-10 rounded-3xl text-center md:text-left relative overflow-hidden z-0">
-        <div className="absolute inset-0 opacity-5 -z-10" style={{
+  <div id="contact" className="py-16 pt-12 lg:py-24 lg:pt-20 pointer-events-auto">
+    <div className="container relative pointer-events-auto">
+      <div className="bg-gradient-to-r from-emerald-300 to-sky-400 text-gray-900 py-8 px-10 rounded-3xl text-center md:text-left relative overflow-hidden">
+      <div className="absolute inset-0 opacity-5 -z-10" style={{
           backgroundImage: `url(${grainImage.src})`
         }}></div>
-    <div className="flex flex-col gap-8 md:gap-16 items-center md:flex-row">
+    <div className="flex flex-col gap-8 md:gap-16 items-center md:flex-row relative z-10">
       <div>
     <h2 className="font-serif text-2xl md:text-3xl">Let&apos;s create something amazing together</h2>
     <p className="text-sm md:text-base mt-2">
       Ready to bring your next project to life? Let&apos;s connect and discuss how I can help you achieve your goals.
     </p>
     </div>
-    <div>
-    <a href="mailto:besirovicelmir36@gmail.com">
-    <button className="text-white bg-gray-900 inline-flex items-center px-6 h-12 rounded-xl gap-2 w-max border border-gray-900 hover:opacity-90 transition">
+    <div className="flex">
+    <a href="mailto:besirovicelmir36@gmail.com" className="text-white bg-gray-900 inline-flex items-center px-6 h-12 rounded-xl gap-2 w-max border border-gray-900 hover:opacity-90 transition pointer-events-auto relative z-50">
       <span className="font-semibold">Contact Me</span>
       <ArrowUpRightIcon className="size-4"/>
-    </button>
     </a>
     </div>
     </div>
